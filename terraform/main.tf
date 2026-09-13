d9e9db950bfef5f2f3845349aa93304395070e06
Osada <lesstaxihr@gmail.com>
2026-09-13 22:31:20 +0530

    Allow HTTP traffic on CloudFront to prevent mixed content errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,7 +145,7 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
