741575e1572e14bd619819315a5e9d308226a818
Osada <lesstaxihr@gmail.com>
2026-09-14 20:57:54 +0530

    Fix CORS policy to allow CloudFront origin

--- backend/src/main/java/com/lesstaxi/todoapp/security/WebSecurityConfig.java
+++ backend/src/main/java/com/lesstaxi/todoapp/security/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
+      configuration.setAllowedOriginPatterns(Arrays.asList("*"));
       configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
       configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
       configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
